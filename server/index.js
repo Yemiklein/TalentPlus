cdb60d75579211082f0752e1e9fdf6f38f680b96
Adeyemi <yemiklein77@gmail.com>
2022-11-15 00:05:50 +0100

    hdh

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
 
-
+app.use(express.json());
 app.use(cors ()); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const port = 5555;
 
-app.get('/', (req, res) => {
-    res.status(200).json({msg: 'TALENT PLUS'})
-});
+app.use(cors());
+app.use(express.json());
 
+app.get("/message", (req, res) => {
+  res.json({ message: "TALENT PLUS" });
+});
 
 
 app.listen(port, () => {
